8f3b1a52b566af240ec3d2d9b591f4cfc4d06dfb
David Black <dblack@atlassian.com>
2021-04-19 14:41:07 +1000

    Permit access to java cacerts.
    Signed-off-by: David Black <dblack@atlassian.com>

--- example-security-manager-core/src/main/java/com/google/security/manas/DefaultSecurityRules.java
+++ example-security-manager-core/src/main/java/com/google/security/manas/DefaultSecurityRules.java
@@ -60,6 +60,8 @@ public class DefaultSecurityRules {
     private static void addMiscPermissions(SecurityPolicy policy) {
         // AWT determines Linux distribution by attempting to read various /etc/*-release files
         policy.addPath("/etc/*", java.awt.GraphicsEnvironment.class.getName(), FileOperation.READ);
+        // java cacerts
+        policy.addPath("/etc/ssl/certs/java/cacerts", FileOperation.READ);
     }
 
     private static void addDevicePermissions(SecurityPolicy policy) {
